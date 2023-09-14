b103467a594f803b96ffbd242c5b7e22e2dd0e7e
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2023-09-14 23:59:11 +0600

    card modified and style added

--- src/assets/Components/Course/Course.jsx
+++ src/assets/Components/Course/Course.jsx
@@ -1,20 +1,22 @@
-
-
+// import { BsBook } from 'react-icons/fa6';
 
 
 const Course = ({course}) => {
     // console.log(course);
     const { name, details, price, credit_hours, img } = course;
+    // console.log(img);
     return (
-        <div className="p-4 border-2 rounded-lg">
+        <div className="p-4 border-2 rounded-lg space-y-3">
             
-            {/* <img src={img} alt="" /> */}
-            <h2>{name}</h2>
+            <img src={img} alt="" />
+            <h2 className="font-semibold text-[1.25rem]">{name}</h2>
+            <div className="text-gray-800 font-semibold space-y-2">
             <p>{details}</p>
-            <div className="flex items-center">
+            <div className="flex items-center justify-between">
                 <p>Price: ${price}</p>
-                <button></button>
-                <p>Credit: {credit_hours}hr</p>
+                {/* <button><BsBook></BsBook></button> */}
+                <p> Credit: {credit_hours}hr</p>
+            </div>
             </div>
             <button className="py-2 w-full bg-blue-700 text-white font-bold rounded-lg text-[1.25rem]">Select</button>
         </div>
